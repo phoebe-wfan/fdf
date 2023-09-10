a781c1e7136e89de3a6129a8ec260e5a98afd15e
Wei FAN <wfan@made-f0Dr9s3.clusters.42paris.fr>
2023-09-10 17:34:19 +0200

    update 10_sep

---
+++ srcs/close.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   close.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wfan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/10 16:41:28 by wfan              #+#    #+#             */
+/*   Updated: 2023/09/10 16:41:35 by wfan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+static void	close_coordinates(t_point **coorfinates, int width);
+
+void	close_all(t_fdf	*fdf, int exit_code)
+{
+	close_coordinares(fdf->map->coordinates, fdf->map->max_x);
+	free(fdf->map);
+	mlx_destroy_image(fdf->mlx, fdf->image->image);
+	free(fdf->image);
+	free(fdf->cam);
+	mlx_destroy_window(fdf->mlx, fdf->win);
+	mlx_destroy_display(fdf->mlx);
+	free(fdf->mlx);
+	free(fdf);
+	error(exit_code);
+}
+
+void	close_map(t_fdf *fdf, int exit_code)
+{
+	close_coordinates(fdf->map->coordinates, fdf->map->max_x);
+	free(fdf->map);
+	mlx_destroy_window(fdf->mlx, fdf->win);
+	mlx_destroy_display(fdf->mlx);
+	free(fdf);
+	error(exit_code);
+}
+
+static void	close_coordinates(t_point **coordinates, int width)
+{
+	int	i;
+
+	i = 0;
+	while (i < width)
+	{
+		free(coordinates[i]);
+		i++;
+	}
+	free(coordinates);
+}
--- srcs/init.c
+++ srcs/init.c
@@ -1,36 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wfan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/03 15:10:09 by wfan              #+#    #+#             */
-/*   Updated: 2023/09/03 16:10:11 by wfan             ###   ########.fr       */
+/*   Created: 2023/09/10 17:33:57 by wfan              #+#    #+#             */
+/*   Updated: 2023/09/10 17:34:00 by wfan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fdf.h"
 
-static int	expose_handle(t_fdf *fdf);
-
-int	main(int ac, char **av)
-{
-	char	*file_name;
-	t_fdf	*fdf;
-
-	if (ac != 2)
-		error(1);
-	file_name = av[1];
-	fdf = init_fdf(file_name);
-	render(fdf);
-	mlx_key_hook(fdf->win, &key_handle, fdf);
-	mlx_expose_hook(fdf->win, &expose_handle, fdf);
-	mlx_loop(fdf->mlx);
-}
-
-static int	expose_handle(t_fdf *fdf)
-{
-	render(fdf);
-	return (0);
-}
