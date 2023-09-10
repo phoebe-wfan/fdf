2268e817997bbd507489ba2e0b0ba9cb0cea9ab5
Wei FAN <wfan@made-f0Dr9s3.clusters.42paris.fr>
2023-09-10 17:35:09 +0200

    update 10_sep

---
+++ includes/colors.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colors.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wfan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/10 15:53:31 by wfan              #+#    #+#             */
+/*   Updated: 2023/09/10 15:53:42 by wfan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLORS_H
+# define COLORS_H
+
+# define C_WHITE	0xffffff
+# define C_GREY		0x303030
+
+# define C_RED		0xff0000
+# define C_GREEN	0x00ff00
+# define C_BLUE		0x0000ff
+
+# define C_YELLOW	0xffff00
+# define C_CYAN		0x00ffff
+# define C_PINK		0xff00ff
+
+# define C_ORANGY	0xffa500
+# define C_BLUEY	0x0492c2
+
+#endif
--- includes/fdf.h
+++ includes/fdf.h
@@ -15,11 +15,149 @@
 
 # include "libft.h"
 # include "mlx.h"
+# include "keys.h"
+# include "colors.h"
 # include <math.h>
 # include <fcntl.h>
 
+typedef struct s_point
+{
+	float	x;
+	float	y;
+	float	z;
+	int		color;
+}	t_point;
 
+typedef struct s_map
+{
+	t_point	**coordinates;
+	int		max_x;
+	int		max_y;
+	int		max_z;
+	int		min_z;
+}	t_map;
 
+typedef struct s_line
+{
+	t_point	start;
+	t_point	end;
+	float	transform_z;
+}	t_line;
 
-# endif
+typedef struct s_color
+{
+	int	start_color;
+	int	start_r;
+	int	start_g;
+	int	start_b;
+	int	end_color;
+	int	end_r;
+	int	end_g;
+	int	end_b;
+	int	delta_r;
+	int	delta_g;
+	int	delta_b;
+}	t_color;
 
+typedef struct s_image
+{
+	void	*image;
+	int		pixel_bits;
+	int		line_bytes;
+	int		endian;
+	char	*buffer;
+	t_line	*line;
+}	t_image;
+
+typedef struct s_cam
+{
+	int		projection;
+	int		color_pallet;
+	float	scale_factor;
+	float	scale_z;
+	float	move_x;
+	float	move_y;
+	double	alpha;
+	double	beta;
+	double	gamma;
+}	t_cam;
+
+typedef struct s_fdf
+{
+	t_map	*map;
+	void	*mlx;
+	void	*win;
+	int		win_x;
+	int		win_y;
+	t_image	*image;
+	t_cam	*cam;
+}	t_fdf;
+
+# define WINDOW_NAME	"fdf"
+# define WINDOW_WIDTH	1200
+# define WINDOW_HEIGHT	900
+# define WINDOW_PIXEL	1080000
+# define LINE_DEFAULT		C_WHITE
+# define BACKGROUND_DEFAULT	C_GREY
+# define C_TEXT				C_WHITE
+# define HEXADECIMAL_L_BASE	"0123456789abcdef"
+# define ANG_1				0.01745329
+# define ANG_30				0.52359877
+# define ANG_45				0.78539816
+
+enum e_projection
+{
+	ISOMETRIC,
+	PERSPECTIVE,
+	TOP
+};
+
+enum e_bool
+{
+	FALSE,
+	TRUE
+};
+
+t_map	*read_map(char *file_name);
+
+void	error(int exit_code);
+
+t_fdf	*init_fdf(char *file_name);
+t_map	*init_map(void);
+t_image	*init_image(void *mlx);
+t_line	*init_line(t_point start, t_point end, t_fdf *fdf);
+t_cam	*init_cam(t_map *map);
+
+t_point	**init_coordinates(int width, int depth);
+void	center_to_origin(t_map *map);
+float	scale_to_fit(t_map *map);
+void	reset(t_fdf *fdf);
+void	close_all(t_fdf *fdf, int exit_code);
+void	close_map(t_fdf *fdf, int exit_code);
+t_color	*color_init(t_point start, t_point end);
+t_color	*color_pallet_init(int min_color, int max_color);
+
+float	absolute(float nbr);
+float	max(float a, float b);
+float	min(float a, float b);
+
+void	render(t_fdf *fdf);
+void	draw_image(t_image *image, int max_x, int max_y);
+void	bresenham(t_fdf *fdf, t_point start, t_point end);
+void	pixel_to_image(t_image *image, float x, float y, int color);
+void	clear_image(t_image *image, int image_size);
+void	print_menu(t_fdf *fdf);
+int		get_color(t_color *color, int i_line, int line_size);
+
+void	rotate(t_cam *cam, t_line *line);
+void	rotate_x(t_line *line, double angle);
+void	rotate_y(t_line *line, double angle);
+void	rotate_z(t_line *line, double angle);
+void	project(t_cam *cam, t_line *line);
+void	transform(t_cam *cam, t_line *line);
+void	scale(t_line *line, int scale_factor);
+void	translate(t_line *line, int move_x, int move_y);
+
+int		key_handle(int keycode, t_fdf *fdf);
+
+#endif
